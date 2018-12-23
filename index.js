4834816d55366524392c1b2fb8d65370c9ca6184
Riccardo H <Ace_Graphics@outlook.de>
2018-12-23 18:03:27 +0100

    fixed role finder

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ dbErrorHandler();
 
 bot.on('guildMemberAdd', member => {
 
-    let role = member.guild.roles.find('name', 'Member'); //looks for the Member role
+    let role = member.guild.roles.find(role => role.name == 'Member'); //looks for the Member role
 
     member.addRole(role); //add the Member role to the user
 
