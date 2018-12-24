21061f50665b1c79b887deb20c8612c873ae8c4e
Riccardo H <Ace_Graphics@outlook.de>
2018-12-24 23:16:07 +0100

    added command unixend

---
+++ commands/unixend.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const colors = require('colors/safe');
+const config = require("../config/bot.json");
+const toDuration = require('humanize-duration');
+
+module.exports.run = async (bot, message, args, prefix) => {
+
+    let time = Math.floor(2147483647 - Math.floor((message.createdAt / 1000))) * 1000;
+    
+    duration = toDuration(time);
+
+    console.log(time);
+
+    let embed = new Discord.RichEmbed()
+        .setTitle('Time until unixend :')
+        .setThumbnail(bot.user.avatarURL)
+        .addField(`Time left`, String(duration), false);
+
+    return message.channel.send(embed);
+
+};
+
+module.exports.help = {
+    name: "unixend"
+};
