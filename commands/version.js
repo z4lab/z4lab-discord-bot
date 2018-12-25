355e3581c564c979ab3ba37420c2d08d5037dcf2
Riccardo H <Ace_Graphics@outlook.de>
2018-12-25 23:02:50 +0100

    added version command

---
+++ commands/version.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const colors = require('colors/safe');
+const { version } = require("discord.js");
+const config = require("../config/bot.json");
+
+
+module.exports.run = async (bot, message, args, prefix) => {
+
+    let embed = new Discord.RichEmbed()
+        .setTitle('z4lab Discord Bot :')
+        .setThumbnail(bot.user.avatarURL)
+        .addField(`Client Version`, `v${config.version.version}`, true)
+        .addField(`Client Build`, config.version.build, true)
+        .addField(`DiscordJS Version`, `v${version}`, true)
+        .addField(`NodeJS Version`, `${process.version}`, true);
+    
+    return message.channel.send(embed);
+
+};
+
+module.exports.help = {
+    name: "version"
+};
