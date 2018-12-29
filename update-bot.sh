589a8fbd1230d3edfa36fb7dc233abc717c0329e
totles <46073304+totles@users.noreply.github.com>
2018-12-29 04:07:36 +0100

    made the script smarter
    smarter update-bot.sh script

--- update-bot.sh
+++ update-bot.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+### CHANGE THIS SETTINGS FOR YOU INSTANCE ###
+INSTALLDIR="/srv"
+TMPDIR="/srv/tmp"
+CURRENTINSTALL="/srv/z4lab-discord-bot"
+### CHANGE THIS SETTINGS FOR YOU INSTANCE ###
+
+# check if tmp directory exists, if not create it
+mkdir -p $TEMPDIR
+
 # backup bot.json file
-cp /srv/z4lab-discord-bot/config/bot.json /srv/tmp/bot.json
-cp /srv/z4lab-discord-bot/config/dbs.json /srv/tmp/dbs.json
+cp $INSTALLDIR/z4lab-discord-bot/config/bot.json $TMPDIR/bot.json
+cp $INSTALLDIR/z4lab-discord-bot/config/dbs.json $TMPDIR/dbs.json
 
 # stop the bot
 forever stopall
 
 # remove current version
-rm -R /srv/z4lab-discord-bot
+rm -R $CURRENTINSTALL
 
 # get new version
-cd /srv/ && git clone https://github.com/totles/z4lab-discord-bot
+cd $INSTALLDIR && git clone https://github.com/totles/z4lab-discord-bot
 
 # move backup config
-mv /srv/tmp/bot.json /srv/z4lab-discord-bot/config/bot.json
-mv /srv/tmp/dbs.json /srv/z4lab-discord-bot/config/dbs.json
+mv $TMPDIR/bot.json $INSTALLDIR/z4lab-discord-bot/config/bot.json
+mv $TMPDIR/dbs.json $INSTALLDIR/z4lab-discord-bot/config/dbs.json
 
 # move directory do npm install and start the bot
-cd /srv/z4lab-discord-bot && npm install && forever start index.js
+cd $INSTALLDIR/z4lab-discord-bot && npm install && forever start index.js
 
 # clear console and print done
 clear
