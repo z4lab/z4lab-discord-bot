3a342c9f3833ff7f4c2ee359ca8db6c2dce957f5
totles <46073304+totles@users.noreply.github.com>
2019-01-03 04:34:01 +0100

    updated bot help page
    are the requires really needed here?

--- commands/help.js
+++ commands/help.js
@@ -1,24 +1,24 @@
 const Discord = require("discord.js");
-const mysql = require("mysql");
+const mysql = require("mysql");         // is this really needed here?
 const colors = require('colors/safe');
-const steam = require('steamidconvert')();
-const SteamAPI = require('steamapi');
+const steam = require('steamidconvert')();  // is this really needed here?
+const SteamAPI = require('steamapi');   // is this really needed here?
 const config = require("../config/bot.json");
-const steamapi = new SteamAPI(config.steam["api-key"]);
+const steamapi = new SteamAPI(config.steam["api-key"]); // is this really needed here?
 
 module.exports.run = function (bot, message, args, prefix){
 
     let embed = new Discord.RichEmbed()
-        .setTitle('z4lab-Bot Commands :')
+        .setTitle('Available commands:')
         .setThumbnail(bot.user.avatarURL)
-        .addField(`${prefix}mtop [map] {username/server(pro/beginner)} {server(pro/beginner)} `, '└ Shows current map/player record', false)
-        .addField(`${prefix}btop [bonusnumber] [map]`, '└ Shows current bonus record', false)
-        .addField(`${prefix}profile [username] {server(pro/beginner)}`, '└ Shows user stats', false)
-        .addField(`${prefix}author`, '└ Shows the authors of the bot', false)
-        .addField(`${prefix}version`, '└ Shows the current bot versions', false)
-        .addField(`${prefix}unixend`, '└ Shows how much time until the world goes down', false)
-        .addField(`${prefix}servers`, '└ Shows the serverlist and advanced server commands', false)
-        .addField(`${prefix}arena`, '└ Shows advanced arena commands', false);
+        .addField(`${prefix}servers`, '⬛ Displays our servers and advanced server commands', false)
+        .addField(`${prefix}arena`, '⬛ Displays advanced arena commands', false)
+        .addField(`${prefix}mtop [map] {username} {server(pro/beginner)} `, '⬛ Shows current map/player records', false)
+        .addField(`${prefix}btop [bonusnumber] [map]`, '⬛ Shows current bonus record', false)
+        .addField(`${prefix}profile [username] {server(pro/beginner)}`, '⬛ Shows user stats', false)
+        .addField(`${prefix}author`, '⬛ Displays the authors of the bot', false)
+        .addField(`${prefix}version`, '⬛ Displays the current bot version', false)
+        .addField(`${prefix}unixend`, '⬛ Displays time till end of the world', false);
 
     return message.channel.send(embed);
 
