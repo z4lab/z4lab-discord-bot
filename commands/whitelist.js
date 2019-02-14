70ad1b9da97647a2f0f009463efd75959f1db989
Riccardo H <Ace_Graphics@outlook.de>
2019-02-14 19:55:01 +0100

    [ * whitelist ] split rm / add permission

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -12,7 +12,7 @@ module.exports.run = function (bot, message, args, prefix, db_beginner, db_pro, 
 
     let access = false;
 
-    whitelist.allowedIDs.forEach(role => {
+    whitelist.allowedIDs.add.forEach(role => {
         if (message.member.roles.has(role)) access = true;
     });
 
@@ -57,45 +57,69 @@ module.exports.run = function (bot, message, args, prefix, db_beginner, db_pro, 
 
         args[1] = args[1].replace('_0', '_1');
 
-            
-            //INSERT INTO `z4labtest`.`mysql_whitelist` (`steamid`) VALUES ('STEAM_1:0:27661072');
-            db_whitelist.query("INSERT INTO `" + db.whitelist.database + "`.`mysql_whitelist` (`steamid`) VALUES ('" + args[1] + "');", function (err) {
-                
-                if (err && err.code != 'ER_DUP_ENTRY') return message.channel.send('```md\n[Error] Failed to whitelist the ID! ]:```').then(console.log(err));
 
-                if (err && err.code == 'ER_DUP_ENTRY') {
+        //INSERT INTO `z4labtest`.`mysql_whitelist` (`steamid`) VALUES ('STEAM_1:0:27661072');
+        db_whitelist.query("INSERT INTO `" + db.whitelist.database + "`.`mysql_whitelist` (`steamid`) VALUES ('" + args[1] + "');", function (err) {
 
-                    steamapi.getUserSummary(steam.convertTo64(String(sid))).then(summary => {
+            if (err && err.code != 'ER_DUP_ENTRY') return message.channel.send('```md\n[Error] Failed to whitelist the ID! ]:```').then(console.log(err));
 
-                        let embed = new RichEmbed()
-                            .setTitle('z4lab Whitelist')
-                            .setDescription('Player is already on the whitelist!')
-                            .setThumbnail(summary.avatar.large)
-                            .addField(`[*] ${summary.nickname}`, summary.url);
-
-                        return message.channel.send(embed);
-
-                    });
-                    return;
-                }
+            if (err && err.code == 'ER_DUP_ENTRY') {
 
                 steamapi.getUserSummary(steam.convertTo64(String(sid))).then(summary => {
 
                     let embed = new RichEmbed()
                         .setTitle('z4lab Whitelist')
-                        .setDescription('Player successfully added to the whitelist!\nsteam://connect/94.130.8.161:27040')
+                        .setDescription('Player is already on the whitelist!')
                         .setThumbnail(summary.avatar.large)
-                        .addField(`[+] ${summary.nickname}`, summary.url);
+                        .addField(`[*] ${summary.nickname}`, summary.url);
 
                     return message.channel.send(embed);
 
                 });
+                return;
+            }
 
+            steamapi.getUserSummary(steam.convertTo64(String(sid))).then(summary => {
+
+                let embed = new RichEmbed()
+                    .setTitle('z4lab Whitelist')
+                    .setDescription('Player successfully added to the whitelist!\nsteam://connect/94.130.8.161:27040')
+                    .setThumbnail(summary.avatar.large)
+                    .addField(`[+] ${summary.nickname}`, summary.url);
+
+                return message.channel.send(embed);
 
             });
 
 
+        });
+
+
     } else if (args[0] == 'rm' || args[0] == 'remove') {
+        let del_access = false;
+        whitelist.allowedIDs.remove.forEach(role => {
+            if (message.member.roles.has(role)) access = true;
+        });
+
+        if (!access) {
+
+            let messageArray = ['Failure is simply the opportunity to begin again, this time more intelligently.',
+                'Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.',
+                'If two wrongs don\'t make a right, try three.',
+                'I am not lazy, I am on energy saving mode.',
+                'People say nothing is impossible, but I do nothing every day.',
+                'The elevator to success is out of order. You’ll have to use the stairs.',
+                'The answer you\'re looking for is inside of you, but it\'s wrong.',
+                'If you think nobody cares if you\'re alive, try missing a couple of payments.',
+                'I thought this would never happen, but you just don\'t care enough for me to give you an answer.',
+                'If you want me to answer that question, you might want to do something better.'
+            ];
+
+            let random = Math.floor(Math.random() * messageArray.length);
+
+            return message.channel.send(messageArray[random]);
+
+        }
         //remove command
         if (!args[1]) return message.channel.send(usage);
 
@@ -104,11 +128,11 @@ module.exports.run = function (bot, message, args, prefix, db_beginner, db_pro, 
         let sid = args[1].replace('_0', '_1') || args[1];
         db_whitelist.query(`SELECT * FROM mysql_whitelist`, function (err, get) {
 
-            for(var i = 0; i < get.length; i++) {
+            for (var i = 0; i < get.length; i++) {
                 if (get[i].steamid == sid) {
                     //DELETE FROM `z4labtest`.`mysql_whitelist` WHERE (`steamid` = 'STEAM_1:0:27661073');
                     db_whitelist.query("DELETE FROM `" + db.whitelist.database + "`.`mysql_whitelist` WHERE (`steamid` = '" + sid + "');", function (err) {
-        
+
                         if (err) console.log(err);
                         steamapi.getUserSummary(steam.convertTo64(String(sid))).then(summary => {
 
@@ -117,9 +141,9 @@ module.exports.run = function (bot, message, args, prefix, db_beginner, db_pro, 
                                 .setDescription('Player successfully removed from the whitelist!')
                                 .setThumbnail(summary.avatar.large)
                                 .addField(`[-] ${summary.nickname}`, summary.url);
-        
+
                             return message.channel.send(embed);
-        
+
                         });
                         return;
                     });
@@ -132,9 +156,9 @@ module.exports.run = function (bot, message, args, prefix, db_beginner, db_pro, 
                                 .setDescription('Player wasn\'t found on the whitelist!')
                                 .setThumbnail(summary.avatar.large)
                                 .addField(`[*] ${summary.nickname}`, summary.url);
-        
+
                             return message.channel.send(embed);
-        
+
                         });
                         return;
                     }
