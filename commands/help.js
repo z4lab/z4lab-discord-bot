516bbbdf683c1f3dc284182411b1c9fff1b1d433
13ace37 <github@xace.ch>
2020-12-20 20:31:33 +0100

    added single command support to help
    .help command - prints command usage

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,9 @@
 const { RichEmbed } = require("discord.js");
+const sql = require("../util/db/sql");
 const getPermissionLvl = require("../util/permissionLvl");
 
-module.exports.run = function (bot, message) {
-
-	let permissionLvl = getPermissionLvl.getUserLevel(message.member);
-
-	let embed = new RichEmbed()
-		.setTitle(`z4lab Discord Bot Commands Overview @ PermLvl ${permissionLvl}`)
-		.setThumbnail(bot.user.avatarURL);
-
+function globalHelp(bot, embed, permissionLvl) {
+	embed.setTitle(`z4lab Discord Bot Commands Overview @ PermLvl ${permissionLvl}`).setThumbnail(bot.user.avatarURL);
 	bot.commands.forEach(command => {
 		if (command.help.category && command.help.category === "main") {
 			if (command.help.permissionLvl <= permissionLvl) {
@@ -23,7 +18,42 @@ module.exports.run = function (bot, message) {
 		}
 
 	});
+	return embed;
+}
 
+module.exports.run = async function (bot, message, args) {
+
+	let permissionLvl = getPermissionLvl.getUserLevel(message.member);
+
+	let embed = new RichEmbed();
+
+	if (args[0]) {
+
+		let cmd = args[0];
+		if (!global.bot.commands.get(cmd)) cmd = await sql.alias.check(bot, cmd);
+		let command = global.bot.commands.get(cmd);
+
+		if (command && permissionLvl >= command.help.permissionLvl) {
+			embed.setTitle(`z4lab Discord Bot ${cmd} usage`).setThumbnail(bot.user.avatarURL);
+			if (!command.help.usage) {
+				embed.addField(`${bot.config.main.prefix}${command.help.name}`, command.help.description || "N/A", false);
+			} else if (command.help.usage && command.help.usage.length == 1) {
+				embed.addField(`${bot.config.main.prefix}${command.help.name} ${command.help.usage[0].command}`, command.help.description || "N/A", false);
+			} else if (command.help.usage && command.help.usage.length > 1) {
+				command.help.usage.forEach((subCommand) => {
+					embed.addField(`${bot.config.main.prefix}${command.help.name} ${subCommand.command}`, subCommand.description || "N/A", false);
+				});
+			} else {
+				embed.addField(`${bot.config.main.prefix}${command.help.name}`, command.help.description || "N/A", false);
+			}
+		} else {
+			embed = globalHelp(bot, embed, permissionLvl);
+		}
+
+	} else {
+		embed = globalHelp(bot, embed, permissionLvl);
+	}
+		
 	return message.channel.send(embed);
 
 };
