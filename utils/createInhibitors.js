08296ad2f105c298aeda89d74afc4d5582d53916
13ace37 <github@xace.ch>
2021-08-10 18:58:16 +0200

    removed optional chaining from array definition

--- utils/createInhibitors.js
+++ utils/createInhibitors.js
@@ -4,12 +4,12 @@ module.exports = () => {
 
 	if (Bot.Settings.channels?.whitelist && Bot.Settings.channels?.blacklist) return Bot.Logger.error("Discord Inhibitors", "Channel black- and whitelist defined! Ignoring all defined channels!");
 	if (Bot.Settings.channels?.whitelist) {
-		if (!Array.isArray(Bot.Settings.channels?.whitelist)) Bot.Settings.channels.whitelist = [Bot.Settings.channels?.whitelist];
+		if (!Array.isArray(Bot.Settings.channels?.whitelist)) Bot.Settings.channels.whitelist = [Bot.Settings.channels.whitelist];
 		Bot.dispatcher.addInhibitor(msg => { if (!Bot.Settings.channels.whitelist.includes(msg.channel.id)) return { reason: "channel not whitelisted" } });
 		Bot.Logger.info("Discord Inhibitors", "Whitelisted channel(s) [%j]", String(Bot.Settings.channels.whitelist).replace(/[||]/g, ""));
 	}
 	if (Bot.Settings.channels?.blacklist) {
-		if (!Array.isArray(Bot.Settings.channels?.blacklist)) Bot.Settings.channels.blacklist = [Bot.Settings.channels?.blacklist];
+		if (!Array.isArray(Bot.Settings.channels?.blacklist)) Bot.Settings.channels.blacklist = [Bot.Settings.channels.blacklist];
 		Bot.dispatcher.addInhibitor(msg => { if (Bot.Settings.channels.blacklist.includes(msg.channel.id)) return { reason: "channel blacklisted" } });
 		Bot.Logger.info("Discord Inhibitors", "Blacklisted channel(s) [%j]", String(Bot.Settings.channels.blacklist).replace(/[||]/g, ""));
 	}
