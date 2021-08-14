a4bce05b3637405e16b3949f46ab3fb4369b4d22
13ace37 <github@xace.ch>
2021-08-14 10:51:24 +0200

    added error messages to mariadb handler

--- utils/createMariaDBConnection.js
+++ utils/createMariaDBConnection.js
@@ -1,7 +1,18 @@
 module.exports = (name, config) => {
 	const Bot = require(process.cwd());
 
-	Bot.DB.MariaDB[name] = require("mariadb").createConnection(config);
+	try {
+		config = JSON.parse(config);
+	} catch (e) {
+		return Bot.Logger.error("MariaDB", "Failed to parse config DB connection [%s]", name);
+	}
+
+	try {
+		Bot.DB.MariaDB[name] = require("mariadb").createConnection(config);
+	} catch (e) {
+		return Bot.Logger.error("MariaDB", "Failed to create DB connection [%s]", name);
+	}
+
 	Bot.Logger.info("MariaDB", "Created DB connection [%s]", name);
 
 }
